89c73f080454087d0da9080476a0fd686b2a4f30
Yohannes Kindu <Yohannes.Kindu@gmail.com>
2022-09-21 10:29:57 +0300

    added routing functionalities, integrated repo with service

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,24 @@
 import { Controller,Get,Post,Body,Param} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 @Controller('/messages')
 export class MessagesController {
+    messageServ:MessagesService
+    constructor(){
+        this.messageServ=new MessagesService();
+    }
     @Get()
     listMessages(){
+        return this.messageServ.findAll()
 
     }
     @Post()
     createMessage(@Body() body:CreateMessageDto){
-        console.log(body)
+        return this.messageServ.create(body.name)
     }
     @Get('/:id')
     listMessage(@Param('id') id:String){
-        console.log(id)
+        return this.messageServ.findOne(id)
 
     }
 
